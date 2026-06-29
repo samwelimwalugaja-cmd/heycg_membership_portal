6c60f98fa0b9d07513494404efd454b67a601483
samwelimwalugaja-cmd <samwelimwalugaja@gmail.com>
2026-06-30 01:15:12 +0300

    update some urls

--- heycg_project/urls.py
+++ heycg_project/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for heycg_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # heycg_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
@@ -23,9 +7,7 @@ from website.views import manage_event_participants
 from website import views
 
 urlpatterns = [
-    # path('grappelli/', include('grappelli.urls')),  # ← Ongeza hii KABLA ya admin
-    
-     path('admin/event-participants/<int:event_id>/', manage_event_participants, name='manage_event_participants'),
+    path('admin/event-participants/<int:event_id>/', manage_event_participants, name='manage_event_participants'),
     path('admin/event-registrations/<int:event_id>/', views.event_registrations_view, name='event_registrations'),
     path('admin/export-event-registrations/<int:event_id>/', views.export_event_registrations, name='export_event_registrations'),
     path('admin/print-event-registrations/<int:event_id>/', views.print_event_registrations, name='print_event_registrations'),
@@ -38,9 +20,6 @@ urlpatterns = [
     path('members/', include('members.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+# Serve media files
 if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
