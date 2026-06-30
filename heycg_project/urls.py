0889448610217dde484f971b4d0b61be0e0148c2
samwelimwalugaja-cmd <samwelimwalugaja@gmail.com>
2026-07-01 00:07:25 +0300

    Update urls.py

--- heycg_project/urls.py
+++ heycg_project/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from website.views import manage_event_participants
 from website import views
 
@@ -20,10 +21,11 @@ urlpatterns = [
     path('members/', include('members.urls')),
 ]
 
-
-# ===== SERVE MEDIA FILES - HII INAFANYA KAZI KILA MAHA! =====
+# ===== SERVE MEDIA FILES - HII INALAZIMISHA KUSERVE HATA KWA PRODUCTION =====
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # Hii ndiyo muhimu: Inalazimisha Django kuserve media hata kwenye production
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
